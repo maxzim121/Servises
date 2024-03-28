c4b85a5ce597b92e24b5e0e9b3ce82750d070bb5
maxzim121 <zimensm12@gmail.com>
2024-03-28 14:54:03 +0300

    Initial Commit

---
+++ Servises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Servises
+//
+//  Created by Maksim Zimens on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
